4cae8c13ef9c0e95cba9b3376c3c3c382b5a861c
harshavardhan <gadde.harshavardhan@oneassist.in>
2025-03-22 05:34:04 +0530

    comment text

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -778,7 +778,7 @@ const Index = () => {
         <div className="max-w-7xl mx-auto">
           <div className="flex justify-between items-center">
             <h1 className="text-2xl font-medium animate-fade-in">Configurable Claims Framework</h1>
-            <div className="text-lg animate-fade-in">Claim Configuration</div>
+            {/* <div className="text-lg animate-fade-in">Claim Configuration</div> */}
           </div>
         </div>
       </header>
